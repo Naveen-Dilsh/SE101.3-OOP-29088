a9fec369c413707c5b1125101c64b36ed1c26516
Naveen-Dilsh <136894087+Naveen-Dilsh@users.noreply.github.com>
2023-07-31 17:48:33 +0530

    Lab

---
+++ Lab 01/Question 2/My.java
@@ -0,0 +1,9 @@
+public class My {
+
+    public static void main(String[] args) {
+		
+        System.out.println("Naveen Kavindhu Dilshan");
+        System.out.println("BSc (Hons) in Computer Science");
+
+    }
+}
